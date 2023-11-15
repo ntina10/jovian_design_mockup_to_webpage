ad5de8bb8a233298f3cd0cc1099953fee54e71c8
ntina10 <frenadin@hotmail.com>
2023-11-15 23:44:01 +0200

    details on background and dropdown functionality

--- script.js
+++ script.js
@@ -1,10 +1,52 @@
-//    onclick="this.src='down.svg'; document.getElementById('txt4').classList.remove('invis')"
-const $toggle = $('.arrow');
+// const btn4 = document.querySelector('.arrow');
+const btn4 = document.getElementById('arrow4');
 
- $toggle.on('click', function() {
-   if ($(this).hasClass('active')) {
-     $(this).removeClass('active');
-   } else {
-     $(this).addClass('active');
-   }
- });
+btn4.addEventListener("click", () => {
+  document.getElementById('txt4').classList.toggle('invis');
+
+  if (btn4.src.indexOf('up.svg')!=-1) {
+      document.getElementById('arrow4').src  = 'down.svg';
+  }
+    else {
+      document.getElementById('arrow4').src = 'up.svg';
+  }
+});
+
+const btn3 = document.getElementById('arrow3');
+
+btn3.addEventListener("click", () => {
+  document.getElementById('txt3').classList.toggle('invis');
+
+  if (btn3.src.indexOf('up.svg')!=-1) {
+      document.getElementById('arrow3').src  = 'down.svg';
+  }
+    else {
+      document.getElementById('arrow3').src = 'up.svg';
+  }
+});
+
+const btn2 = document.getElementById('arrow2');
+
+btn2.addEventListener("click", () => {
+  document.getElementById('txt2').classList.toggle('invis');
+
+  if (btn2.src.indexOf('up.svg')!=-1) {
+      btn2.src  = 'down.svg';
+  }
+    else {
+      btn2.src = 'up.svg';
+  }
+});
+
+const btn1 = document.getElementById('arrow1');
+
+btn1.addEventListener("click", () => {
+  document.getElementById('txt1').classList.toggle('invis');
+
+  if (btn1.src.indexOf('up.svg')!=-1) {
+      document.getElementById('arrow1').src  = 'down.svg';
+  }
+    else {
+      document.getElementById('arrow1').src = 'up.svg';
+  }
+});
